0cc36e3f77be0de4a2938d2ed88eb39640f08a92
Luke <lukebender304@gmail.com>
2025-07-11 12:09:59 -0400

    fix

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ console.log("Normalized areaCode:", areaCode);
   const time = parseFloat((hours + minutes / 60).toFixed(2));
 
   // Add areaCode to the return for debugging
-  return res.json({ city, time, areaCode, rawAreaCodemmit });
+  return res.json({ city, time, areaCode, rawAreaCode });
 });
 
 
