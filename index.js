9b7271b8ae82cf75c609ccfea49209734a60ac22
Luke <lukebender304@gmail.com>
2025-07-13 04:32:07 -0400

    g

--- index.js
+++ index.js
@@ -28,12 +28,13 @@ app.post('/get-article', async (req, res) => {
     .replace(/-+/g, '-');     // collapse multiple dashes
   console.log(lower_title);
   const url = ARTICLE_PREFIX + lower_title + '/';
-   try {
+    try {
     const response = await axios.get(url);
     const html = response.data;
     console.log(html);
   } catch (error) {
     console.error('Error fetching HTML:', error.message);
+  }
   let content = '';
   try {
   const dom     = new JSDOM(html, { url });
